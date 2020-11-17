5cbf26bcb076d7c62db939017d55488617a58675
kanav chadha <kanavchadha56@gmail.com>
2020-11-18 01:04:14 +0530

    initial commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
 
     app.use(proxy("/api", {
-        target: "http://localhost:5000/",
+        // target: "http://localhost:5000/",
+        target: "https://codingchatapp.herokuapp.com/",
         changeOrigin: true,
     }));
 
